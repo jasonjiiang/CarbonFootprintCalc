ae786854df524ab1eabb8f03dc41974631f677b7
jasonjiiang <52567513+jasonjiiang@users.noreply.github.com>
2023-03-14 15:43:26 +0000

    Extra features
    Added labels for the results of carbon emissions of each section. Added radio buttons in the "Transport" section for multiple cars (will work more on this later). Reverted and cleaned up some of the functions.

--- src/code.js
+++ src/code.js
@@ -11,37 +11,27 @@ function changeLayout(tab)
     changeTab(tab)
     if (currentTab != prevTab)
     {
-        changeTab();
-        changeCalc();
+        //Changes tab bar
+        document.getElementById(currentTab+"-button").classList.add("active");
+        document.getElementById(prevTab+"-button").classList.remove("active");
+        //Changes calculator form
+        document.getElementById(currentTab+"-form").classList.add("d-block");
+        document.getElementById(currentTab+"-form").classList.remove("d-none");
+        document.getElementById(prevTab+"-form").classList.add("d-none");
+        document.getElementById(prevTab+"-form").classList.remove("d-block");
     }
 }
 
-function changeTab()
+function extraForm(vis, div)
 {
-    //Changes tab bar
-    document.getElementById(currentTab+"-button").classList.add("active");
-    document.getElementById(prevTab+"-button").classList.remove("active");
-}
-
-function changeCalc()
-{
-    //Changes calculator form
-    document.getElementById(currentTab+"-form").classList.add("d-block");
-    document.getElementById(currentTab+"-form").classList.remove("d-none");
-    document.getElementById(prevTab+"-form").classList.add("d-none");
-    document.getElementById(prevTab+"-form").classList.remove("d-block");
-}
-
-function enableFlight()
-{
-    document.getElementById("flights-taken").classList.add("d-block");
-    document.getElementById("flights-taken").classList.remove("d-none");
-}
-
-function disableFlight()
-{
-    document.getElementById("flights-taken").classList.add("d-none");
-    document.getElementById("flights-taken").classList.remove("d-block");
+    if (vis)
+    {
+        document.getElementById(div).classList.add("d-block");
+        document.getElementById(div).classList.remove("d-none");
+    } else {
+        document.getElementById(div).classList.add("d-none");
+        document.getElementById(div).classList.remove("d-block");
+    }
 }
 
 window.onload = function()
